be1711aeebffd9fa18a1d376b438063222cae49d
Matt Grandy <mgrandy@fortynorthsecurity.com>
2020-05-01 07:52:20 -0600

    Added functionality to handle RDP issues

--- Screenshooter/Program.cs
+++ Screenshooter/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Threading;
 
 class Program
 {
@@ -12,7 +15,6 @@ class Program
 
     private static void Shooter(string filePath)
     {
-
         // Screenshot block to screenshot Multiple screens
         SetProcessDPIAware();
         Bitmap screenshot = new Bitmap(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height,
@@ -23,13 +25,31 @@ class Program
                                     CopyPixelOperation.SourceCopy);
 
         screenshot.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
-
-
+        Console.WriteLine("[+] Screenshot successful, exiting now");
     }
+
+
+    [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+    static extern bool ProcessIdToSessionId(uint dwProcessId, out uint pSessionId);
+
+    static uint GetTerminalServicesSessionId()
+    {
+        var proc = Process.GetCurrentProcess();
+        var pid = proc.Id;
+
+        var sessionId = 0U;
+        if (ProcessIdToSessionId((uint)pid, out sessionId))
+        {
+            return sessionId;
+        }
+        return 1U; // fallback, the console session is session 1
+    }
+
     public static void Main(string[] args)
     {
         // Checks for too many args -> creates a string from array -> checks for no args -> checks if the file already exists -> screenshot
         string filePath;
+        bool isElevated;
         string appPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
         if (args.Count() > 1)
@@ -44,7 +64,7 @@ class Program
 
         if (filePath == null || filePath.Count() == 0)
         {
-            filePath = appPath + DateTime.Now.ToString("M-dd-yyyy--HH-mm-ss") + ".png";
+            filePath = appPath + "\\" + DateTime.Now.ToString("M-dd-yyyy--HH-mm-ss") + ".png";
         }
         else if (File.Exists(filePath))
         {
@@ -77,10 +97,47 @@ class Program
         {
             Shooter(filePath);
         }
+        catch (System.ComponentModel.Win32Exception a)
+        {
+            Console.WriteLine("[+] User may be running inside RDP session, attempting to attach with tscon...\n");
+
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+
+            if(isElevated)
+            {
+                Console.WriteLine("[+] User is admin, continuing...");
+                uint sessionID = GetTerminalServicesSessionId();
+                if (sessionID == 0)
+                {
+                    Console.WriteLine("\n[-] Session ID is 0 which means you're probably running as SYSTEM");
+                    Console.WriteLine("[-] Try running as user in a high integrity context");
+                }
+                else
+                {
+                    //Grab the session ID for the current process
+                    var winDir = System.Environment.GetEnvironmentVariable("WINDIR");
+                    Process.Start(Path.Combine(winDir, "system32", "tscon.exe"),
+                        String.Format("{0} /dest:console", GetTerminalServicesSessionId()))
+                    .WaitForExit();
+                    Thread.Sleep(1000);
+
+                    Shooter(filePath);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n[-] Not running with Administrator privileges, please rerun as admin");
+                return;
+            }           
+        }
         catch (System.Runtime.InteropServices.ExternalException)
         {
             Console.WriteLine("[-] Error with path given (unable to save bitmap there)\n");
-            filePath = appPath + DateTime.Now.ToString("M-dd-yyyy--HH-mm-ss") + ".png";
+            filePath = appPath + "\\" + DateTime.Now.ToString("M-dd-yyyy--HH-mm-ss") + ".png";
             Console.WriteLine("[+] Using default screenshot location: " + Path.GetFullPath(filePath));
             Shooter(filePath);
         }
